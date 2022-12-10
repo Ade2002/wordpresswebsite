937242221f3a975ff8175df6ab853cee1cd3dd1c
Ade2002 <sikirusulaimon2002@gmail.com>
2022-12-10 07:54:28 +0100

    update

---
+++ assets/js/contact.js
@@ -0,0 +1,15 @@
+let inputs = document.querySelectorAll("input");
+inputs.forEach((input) =>
+  input.addEventListener("change", (e) => {
+    label = e.target.closest(".userBox").querySelector("label");
+    if (input.value != "") {
+      label.style.top = "-20px";
+      label.style.color = "#" + "03e9f4";
+      label.style.fontSize= "12px";
+    }else{
+        label.style.top = "0px";
+        label.style.fontSize= "18px";
+        label.style.color = "white";
+    }
+  }),
+);
